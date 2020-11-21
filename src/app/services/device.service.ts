2beb4e54ff9cd9ac7f9de7b253b8dc453fc6c9ce
FahiM <fahim@FahiMs-MacBook-Pro.local>
2020-11-20 22:02:57 -0500

    device & packages service is done

--- src/app/services/device.service.ts
+++ src/app/services/device.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { HeaderRenderer } from '@syncfusion/ej2-angular-schedule';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceService {
 
-  constructor() { }
+  device_api = "http://localhost:3000";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllDevices(){
+    return this.http.get<[]>(this.device_api+'/getdevices');
+  }
+
+  getDevice(id){
+    return this.http.get<[]>(this.device_api+"/getdevice/"+id);
+  }
+
+  postDevice(obj){
+    return this.http.post<[]>(this.device_api+"/newdevice/", obj);
+  }
+
+  updateDevice(id, obj){
+    return this.http.post<[]>(this.device_api+"/updatedevice/"+id, obj);
+  }
+
 }
--- src/app/services/package.service.ts
+++ src/app/services/package.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PackageService {
 
-  constructor() { }
+  package_api = "http://localhost:3000"
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllPackages(){
+    return this.http.get<[]>(this.package_api+"/getpackages");
+  }
+
+  getPackage(id){
+    return this.http.get<[]>(this.package_api+"/getpackage/", id);
+  }
+
+  newPackage(obj){
+    return this.http.post<[]>(this.package_api+"/newpackage/", obj);
+  }
+
+  updatePackage(id, obj){
+    return this.http.post<[]>(this.package_api+"/updatepackage/"+id, obj);
+  }
 }
