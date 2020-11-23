077b62a850bca3adea712a76a348883e9e11020d
FahiM <fahimalsafi@gmail.com>
2020-11-22 19:16:38 -0500

    package sorting options

--- src/app/package/package-list/package-list.component.ts
+++ src/app/package/package-list/package-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,FormControl,FormBuilder,Validator,Validators, FormArray
+  } from '@angular/forms';
+  import {PackageService} from "../../services/package.service";
 
 @Component({
   selector: 'app-package-list',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackageListComponent implements OnInit {
 
-  constructor() { }
+  sortForm : FormGroup;
+  searchForm: FormGroup;
+  package_type = [
+    {id: 0, name : "Internet", value:"Internet"},
+    {id: 1, name: "TV", value:"TV"},
+    {id: 2, name:"Phone", value:"Phone"}
+  ];
+
+  constructor(
+    private fb: FormBuilder,
+    private ps : PackageService
+  ) { }
 
   ngOnInit(): void {
+    this.formInit();
+    this.getPackages();
+  }
+
+  getPackages(){
+    this.ps.getAllPackages().toPromise()
+    .then((res:any)=>{
+      if(res.status == "ok"){
+        console.log(res.data);
+      } else {
+        console.log(res);
+      }
+    })
+  }
+
+  formInit(){
+    this.sortForm = this.fb.group({
+      type : ['']
+    })
+
+    this.searchForm = this.fb.group({
+      str : [""]
+    })
+  }
+
+  onChangeType(){
+
+  }
+
+  countPackage(){
+
+  }
+
+  onSearchString(){
+
+  }
+
+  newPackage(){
+    
   }
 
 }
